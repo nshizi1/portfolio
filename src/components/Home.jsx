4b03753e87e9dd16ab7bf02c913b75918986e814
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2023-12-28 19:35:56 -0800

    hero section development

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 function Home() {
   return (
     <main>
-      <div id="hero"></div>
+      <section id="hero">
+        <div className="contents">
+          <div className="intro">
+            <div className="texts">
+              <h1>Wilson Nshizirungu</h1>
+              <div className="desc">
+                <h3>Fullstack Website developer</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsum neque illum, fuga sint harum culpa id earum expedita eveniet est, a deleniti quibusdam asperiores reiciendis minima itaque fugiat quod.</p>
+              </div>
+              <a href="#about" className="btn-get-started">Say hello</a>
+            </div>
+            <div className="image">
+              <img src={require("../images/hero.jpg")} alt="Developer's profile" />
+            </div>
+          </div>
+          <div className="social">
+          </div>
+          <div className="scroll">
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
