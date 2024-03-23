68a494a387f0d1777c782ebe0d4959b623c73ea2
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-23 03:45:06 +0200

    development

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import ErrorPage from './components/ErrorPage';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -17,6 +18,7 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <p className="text-blue-500 underline">Hello Tailwind CSS!</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
