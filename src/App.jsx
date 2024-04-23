bda8cb19e35bb3cc887c3b09c9732e7dc1c822fb
nshizirungu wilson <121226507+nshizi1@users.noreply.github.com>
2024-04-23 21:28:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
@@ -8,21 +8,34 @@ import ErrorPage from './components/ErrorPage';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-
 function App() {
-  useEffect(()=> {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
     AOS.init();
+
+    window.addEventListener('load', () => {
+      setLoading(false);
+    });
   }, []);
+
   return (
     <BrowserRouter>
-      <Header />
+      {loading && (
+        <div id="loading-screen" style={{ position: 'fixed', zIndex: 9999, top: 0, left: 0, width: '100%', height: '100%', background: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', transition: 'opacity 0.5s ease' }}>
+          <h1>Loading...</h1>
+        </div>
+      )}
+      <div style={{ opacity: loading ? 0 : 1, transition: 'opacity 0.5s ease' }}>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
-      <Footer />
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
